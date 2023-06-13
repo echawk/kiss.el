d0e0b062f3e02955f22bae28b3b070ce48c1b094
Ethan <ethan.hawk@valpo.edu>
2023-06-13 18:26:33 -0500

    kiss.el: add implementation of kiss/preferred

--- kiss.el
+++ kiss.el
@@ -185,6 +185,49 @@
                (kiss/internal--get-installed-manifest-files)))))
     "/")))
 
+(defun kiss/preferred ()
+  (mapcar
+   ;; NOTE: this may split files with ':' in the name...
+   ;; Do the final split of package and file.
+   (lambda (s) (string-split s ":"))
+   ;; Split up each line from each other.
+   (string-split
+    ;; Clean up the string to just contain the package name
+    ;; on the left and the file on the right.
+    (replace-regexp-in-string
+     kiss/installed-db-dir ""
+     (replace-regexp-in-string
+      "/manifest:" ":" ;; NOTE: change this to be a diff character
+
+
+      (shell-command-to-string
+       (concat
+        ;; Use printf since it's the closest thing we have to being
+        ;; able to pipe without using mkfifo(1)
+        "printf '"
+        ;; TODO: think about factoring this code out.
+        (mapconcat
+         #'identity
+         (delete-dups
+          (mapcar
+           (lambda (file-str)
+             (concat
+              "/"
+              (mapconcat #'identity
+                         (cdr (split-string file-str ">"))
+                         "/")))
+           (nthcdr 2 (directory-files kiss/choices-db-dir))))
+         "\\n")
+        "'"
+        ;; Now for the piping into grep.
+        " | "
+        "grep -Fxf - "
+        (kiss/internal--lst-to-str
+         (kiss/internal--get-installed-manifest-files))
+        " /dev/null"
+        ))))
+    "\n"))
+  )
 ;; -> build        Build packages
 ;; ===========================================================================
 (defun kiss/build (pkgs-l)
